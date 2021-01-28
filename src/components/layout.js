1dd3a12ab84615ad899aaa77b9d57cd5c10bd47c
Damian Wilkołek <dwilkolek@protonmail.com>
2021-01-28 20:55:23 +0100

    upgrade deps & redeploy

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,40 +12,16 @@ import PropTypes from "prop-types"
 import Footer from "./footer"
 import "./bulma.scss"
 
-import CookieConsent from 'react-cookie-consent';
-
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faLinkedinIn, faGithub, faStrava } from '@fortawesome/free-brands-svg-icons';
 import { faCode, faDatabase, faCubes, faCheckDouble, faTools, faGrinBeam, faTrophy, faHatCowboy } from '@fortawesome/free-solid-svg-icons'
 library.add(faLinkedinIn, faGithub, faStrava, faCode, faDatabase, faCubes, faCheckDouble, faTools, faGrinBeam, faTrophy, faHatCowboy);
 
 const Layout = ({ children }) => {
-  // const data = useStaticQuery(graphql`
-  //   query SiteTitleQuery {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `)
-
   return (
     <>
       <main>{children}</main>
       <Footer />
-      {/* <CookieConsent
-        location="bottom"
-        buttonText="Accept"
-        declineButtonText="Decline"
-        cookieName="gatsby-gdpr-google-analytics"
-        style={{ background: "#2B373B" }}
-        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
-        expires={150}
-        enableDeclineButton={true}
-      >
-        This website uses cookies to enhance the user experience.{" "}  
-      </CookieConsent> */}
     </>
   )
 }
